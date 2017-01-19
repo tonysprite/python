338fc5d74a758241a5035c8f6f99c05df8fff3ab
tonysprite <catchyouhand@qq.com>
2017-01-20 06:27:37 +0800

    learning python data type list

---
+++ python_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+like_js_array_list=["a",2,'b','c'];
+print like_js_array_list;
+print "print the first char in list \n\r";
+print like_js_array_list[0];
+print "print the 1 to 3 char in list \n\r";
+print like_js_array_list[0:2];
+print "\n\ryou`ve found that the third element didn`t apear in the result."
+
