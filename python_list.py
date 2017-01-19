f43fdab5eda44dfdcc1bb01e5f3e7b9161d8a5ec
tonysprite <catchyouhand@qq.com>
2017-01-20 06:32:39 +0800

    merge list in python

--- python_list.py
+++ python_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 like_js_array_list=["a",2,'b','c'];
+list_two=['cc','bb'];
 print like_js_array_list;
 print "print the first char in list \n\r";
 print like_js_array_list[0];
 print "print the 1 to 3 char in list \n\r";
 print like_js_array_list[0:2];
-print "\n\ryou`ve found that the third element didn`t apear in the result."
-
+print "\n\ryou`ve found that the third element didn`t apear in the result.\n\r"
+print like_js_array_list + list_two;
+print "\n\r you`ve found that the first list merged with the second list "
